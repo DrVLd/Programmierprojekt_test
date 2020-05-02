a34d5a2ac72a71f500f5365175ee7b7f1c36353c
Sergeton <htwtest30@gmail.com>
2020-05-02 10:41:40 +0200

    Initial commit

---
+++ Programmierprojekt_test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmierprojekt_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Herzlich Willkommen in Programmierprojekt");
+            Console.ReadKey();
+        }
+    }
+}
